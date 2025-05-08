dfa903e151e52108a201b3bb076d9249d3d6cbe3
madhav23484 <madhav23484@gmail.com>
2025-05-08 15:11:49 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? _image;
+  final picker = ImagePicker();
+  String _imagePath = 'No image selected';
+
+  Future getImage(ImageSource source) async {
+    final pickedFile = await picker.pickImage(source: source);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+        _imagePath = pickedFile.path;
+      } else {
+        print('No image selected.');
+        _imagePath = 'No image selected';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Image Path:',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            SizedBox(height: 10),
+            Text(_imagePath),
+            SizedBox(height: 20),
+            _image == null
+                ? Text('No image selected.')
+                : Image.file(_image!, height: 200),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: () => getImage(ImageSource.camera),
+                  tooltip: 'Pick Image from Camera',
+                  child: const Icon(Icons.camera_alt),
+                ),
+                FloatingActionButton(
+                  onPressed: () => getImage(ImageSource.gallery),
+                  tooltip: 'Pick Image from Gallery',
+                  child: const Icon(Icons.photo_library),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
